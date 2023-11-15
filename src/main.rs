b586db54d60f568829ea599fc16903f1ec72d737
Robert <robert.nolte@andrena.de>
2023-11-15 15:31:37 +0100

    Add timeout handler

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use axum::{routing::get, Router};
 use std::time::Duration;
 
+use axum::{error_handling::HandleErrorLayer, routing::get, BoxError, Router};
+use hyper::StatusCode;
+use tower::ServiceBuilder;
 
 #[tokio::main]
 async fn main() {
-
     let app = construct_app();
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -22,8 +23,23 @@ fn construct_app() -> Router {
                 "10 seconds have passed: you may continue with other task now"
             }),
         )
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(handle_timeout_error))
+                .timeout(Duration::from_secs(1)),
+        )
 }
 
+async fn handle_timeout_error(err: BoxError) -> (StatusCode, &'static str) {
+    if err.is::<tower::timeout::error::Elapsed>() {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Handler has taken too long",
+        )
+    } else {
+        (StatusCode::OK, "")
+    }
+}
 #[cfg(test)]
 mod tests {
     use crate::construct_app;
