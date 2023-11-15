1515c897f34fd4741f56a14ab3c9e4ca4a367a45
Robert <robert.nolte@andrena.de>
2023-11-15 14:56:37 +0100

    Add test for root route

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 use axum::{routing::get, Router};
 
+
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route(
-        "/",
-        get(|| async {
-            "Hello, World!"
-        }),
-    );
 
-    // run it with hyper on localhost:3000
+    let app = construct_app();
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+fn construct_app() -> Router {
+    Router::new().route(
+        "/",
+        get(|| async {
+            "Hello, World!"
+        }),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::construct_app;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use tower::ServiceExt; 
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = construct_app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
 }
