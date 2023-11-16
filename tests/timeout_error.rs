44acbc73462fb5f6d382ef26cad55f96d89fbfe0
Robert <robert.nolte@andrena.de>
2023-11-16 12:12:38 +0100

    Arrange Act Assert

--- tests/timeout_error.rs
+++ tests/timeout_error.rs
@@ -4,19 +4,19 @@ use axum::{
 };
 use hands_on_lib::construct_app;
 use tower::util::ServiceExt;
+
 #[tokio::test]
 async fn timeout() {
+    //arrange
     let app = construct_app();
 
-    let response = app
-        .oneshot(
-            Request::builder()
-                .uri("/10_seconds_timer")
-                .body(Body::empty())
-                .unwrap(),
-        )
-        .await
+    let request = Request::builder()
+        .uri("/10_seconds_timer")
+        .body(Body::empty())
         .unwrap();
+    //act
+    let response = app.oneshot(request).await.unwrap();
 
+    //assert
     assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
 }
