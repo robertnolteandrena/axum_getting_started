c3f492388df2e7ba5c90a0b3b9013ff9e335e3d5
Robert <robert.nolte@andrena.de>
2023-11-16 10:11:47 +0100

    lib bin split

---
+++ src/bin.rs
@@ -0,0 +1,10 @@
+use hands_on_lib::construct_app;
+
+#[tokio::main]
+async fn main() {
+    let app = construct_app();
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use std::time::Duration;
+
+use axum::{error_handling::HandleErrorLayer, routing::get, BoxError, Router};
+use hyper::StatusCode;
+use tower::ServiceBuilder;
+
+pub fn construct_app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/10_seconds_timer",
+            get(|| async {
+                tokio::time::sleep(Duration::from_secs(10)).await;
+                "10 seconds have passed: you may continue with other task now"
+            }),
+        )
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(handle_timeout_error))
+                .timeout(Duration::from_secs(1)),
+        )
+}
+
+pub async fn handle_timeout_error(err: BoxError) -> (StatusCode, &'static str) {
+    if err.is::<tower::timeout::error::Elapsed>() {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Handler has taken too long",
+        )
+    } else {
+        (StatusCode::OK, "")
+    }
+}
