10be29d0ce732a2ba197982e6926be4e640f0462
Robert <robert.nolte@andrena.de>
2023-11-16 09:36:46 +0100

    Moving unit test into lib

--- src/bin.rs
+++ src/bin.rs
@@ -9,28 +9,3 @@ async fn main() {
         .unwrap();
 }
 
-//Unit test
-#[cfg(test)]
-mod tests {
-    use crate::construct_app;
-    use axum::{
-        body::Body,
-        http::{Request, StatusCode},
-    };
-    use tower::ServiceExt;
-
-    #[tokio::test]
-    async fn hello_world() {
-        let app = construct_app();
-
-        let response = app
-            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
-            .await
-            .unwrap();
-
-        assert_eq!(response.status(), StatusCode::OK);
-
-        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
-        assert_eq!(&body[..], b"Hello, World!");
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,30 @@ use axum::{routing::get, Router};
 pub fn construct_app() -> Router {
     Router::new().route("/", get(|| async { "Hello, World!" }))
 }
+
+//Unit test
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    use crate::construct_app;
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = construct_app();
+
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+}
