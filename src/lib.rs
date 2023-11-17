1ad34ecb5f97f846166573ca33b89f1e27364a6c
Robert <robert.nolte@andrena.de>
2023-11-17 13:32:52 +0100

    Refactor Middleware

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,16 @@
 pub mod controllers;
 pub mod dto;
+pub mod layers;
 use std::time::Duration;
 
-use axum::{error_handling::HandleErrorLayer, BoxError, Router};
+use axum::Router;
 use controllers::temperature::get_temperature_routes;
-use hyper::StatusCode;
-use tower::ServiceBuilder;
+use layers::timeout_layer::add_timeout_handler;
 
 pub const TIMER_URI: &str = "/10_seconds_timer";
 
 pub fn construct_app() -> Router {
-    Router::new()
-        .nest("/temperature", get_temperature_routes())
-        .layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(handle_timeout_error))
-                .timeout(Duration::from_secs(1)),
-        )
-}
-
-pub async fn handle_timeout_error(err: BoxError) -> (StatusCode, &'static str) {
-    if err.is::<tower::timeout::error::Elapsed>() {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Handler has taken too long",
-        )
-    } else {
-        (StatusCode::OK, "")
-    }
+    let mut app = Router::new().nest("/temperature", get_temperature_routes());
+    app = add_timeout_handler(app, Duration::from_secs(1));
+    app
 }
