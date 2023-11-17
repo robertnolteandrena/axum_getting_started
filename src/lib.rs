d137f2e7e86d90bc5448ad9d164418f75d8acef2
Robert <robert.nolte@andrena.de>
2023-11-17 15:15:23 +0100

    Use route

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,13 @@ pub mod layers;
 use std::time::Duration;
 
 use axum::Router;
-use controllers::temperature::get_temperature_routes;
+use controllers::{header_controller::get_header_routes, temperature::get_temperature_routes};
 use layers::timeout_layer::add_timeout_handler;
 
 pub fn construct_app() -> Router {
-    let mut app = Router::new().nest("/temperature", get_temperature_routes());
+    let mut app = Router::new()
+        .nest("/temperature", get_temperature_routes())
+        .nest("/header", get_header_routes());
     app = add_timeout_handler(app, Duration::from_secs(1));
     app
 }
