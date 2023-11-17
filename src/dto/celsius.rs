96e809e955295afb1f0023890d015d6116bf90c0
Robert <robert.nolte@andrena.de>
2023-11-17 10:22:46 +0100

    Restructure temperatures

--- src/dto/celsius.rs
+++ src/dto/celsius.rs
@@ -6,9 +6,3 @@ use super::fahrenheit::Fahrenheit;
 pub struct Celsius {
     pub celsius_value: f32,
 }
-impl From<Celsius> for Fahrenheit {
-    fn from(val: Celsius) -> Self {
-        let fahrenheit_value = val.celsius_value * 9f32 / 5f32 + 32f32;
-        Fahrenheit { fahrenheit_value }
-    }
-}
--- src/dto/fahrenheit.rs
+++ src/dto/fahrenheit.rs
@@ -6,9 +6,9 @@ use super::celsius::Celsius;
 pub struct Fahrenheit {
     pub fahrenheit_value: f32,
 }
-impl From<Fahrenheit> for Celsius {
-    fn from(val: Fahrenheit) -> Self {
-        let celsius_value = (val.fahrenheit_value - 32f32) * 5f32 / 9f32;
-        Celsius { celsius_value }
+impl From<Celsius> for Fahrenheit {
+    fn from(val: Celsius) -> Self {
+        let fahrenheit_value = val.celsius_value * 9f32 / 5f32 + 32f32;
+        Fahrenheit { fahrenheit_value }
     }
 }
