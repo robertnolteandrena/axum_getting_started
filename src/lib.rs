7ad1de7abdb6b7363892116704433a29631a7138
Robert <robert.nolte@andrena.de>
2023-11-17 13:34:55 +0100

    Remove unused constant

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,6 @@ use axum::Router;
 use controllers::temperature::get_temperature_routes;
 use layers::timeout_layer::add_timeout_handler;
 
-pub const TIMER_URI: &str = "/10_seconds_timer";
-
 pub fn construct_app() -> Router {
     let mut app = Router::new().nest("/temperature", get_temperature_routes());
     app = add_timeout_handler(app, Duration::from_secs(1));
