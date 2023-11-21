4ddc0aa118a0fe7b9188f309432cc1897bd20112
Robert <robert.nolte@andrena.de>
2023-11-21 09:30:49 +0100

    Rename temperature_controller

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,2 +1,2 @@
 pub mod header_controller;
-pub mod temperature;
+pub mod temperature_controller;
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ pub mod layers;
 use std::time::Duration;
 
 use axum::Router;
-use controllers::{header_controller::get_header_routes, temperature::get_temperature_routes};
+use controllers::{
+    header_controller::get_header_routes, temperature_controller::get_temperature_routes,
+};
 use layers::timeout_layer::add_timeout_handler;
 
 pub fn construct_app() -> Router {
