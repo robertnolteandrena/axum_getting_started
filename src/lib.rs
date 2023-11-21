e275b8452d4bf4713d3f35ed05413a2e69a505e6
Robert <robert.nolte@andrena.de>
2023-11-21 16:15:04 +0100

    Move layers into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 pub mod controllers;
 pub mod dto;
-pub mod layers;
 use std::time::Duration;
 
-use axum::Router;
+use axum::{error_handling::HandleErrorLayer, BoxError, Router};
 use controllers::temperature_controller::get_temperature_routes;
-use layers::timeout_layer::add_timeout_handler;
+use hyper::StatusCode;
+use tower::ServiceBuilder;
 
 pub fn construct_app() -> Router {
-    let mut app = Router::new().nest("/temperature", get_temperature_routes());
-    app = add_timeout_handler(app, Duration::from_secs(1));
-    app
+    Router::new()
+        .nest("/temperature", get_temperature_routes())
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(handle_timeout_error))
+                .timeout(Duration::from_secs(10)),
+        )
+}
+async fn handle_timeout_error(err: BoxError) -> (StatusCode, &'static str) {
+    if err.is::<tower::timeout::error::Elapsed>() {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Handler has taken too long",
+        )
+    } else {
+        (StatusCode::OK, "")
+    }
 }
