3658ad0113bfe5c97e0e2ca02b68e42c6f47a476
Robert <robert.nolte@andrena.de>
2023-11-21 16:10:28 +0100

    Generic layer

--- tests/exploratory_tower_middleware.rs
+++ tests/exploratory_tower_middleware.rs
@@ -1,10 +1,10 @@
-use std::convert::Infallible;
+use std::{convert::Infallible, fmt::Display};
 
 use spectral::assert_that;
 use tower::{ServiceBuilder, ServiceExt};
 
-async fn my_service(request: String) -> Result<String, Infallible> {
-    let response = format!("my_service({})", request.to_owned());
+async fn my_service<T: Display>(request: T) -> Result<String, Infallible> {
+    let response = format!("my_service({})", request);
     Ok(response)
 }
 
@@ -13,18 +13,18 @@ async fn middleware_with_one_service() {
     //a service consumes a request and returns a response
     let svc = tower::service_fn(my_service);
     let sb = ServiceBuilder::new().service(svc);
-    let response = sb.oneshot("Vanilla request".to_owned()).await.unwrap();
-    assert_that!(response).is_equal_to("my_service(Vanilla request)".to_owned());
+    let response = sb.oneshot("Vanilla request").await.unwrap();
+    assert_that!(response.as_ref()).is_equal_to("my_service(Vanilla request)");
 }
 
 #[tokio::test]
 async fn middleware_with_one_service_and_one_layer() {
     let svc = tower::service_fn(my_service);
     let sb = ServiceBuilder::new()
-        .map_request(|request: String| format!("map_request({})", request))
+        .map_request(|request: &str| format!("map_request({})", request))
         .service(svc);
-    let response = sb.oneshot("Vanilla request".to_owned()).await.unwrap();
-    assert_that!(response).is_equal_to("my_service(map_request(Vanilla request))".to_owned());
+    let response = sb.oneshot("Vanilla request").await.unwrap();
+    assert_that!(response.as_ref()).is_equal_to("my_service(map_request(Vanilla request))");
 }
 
 #[tokio::test]
@@ -33,6 +33,16 @@ async fn middleware_with_one_service_and_mapresponse() {
     let sb = ServiceBuilder::new()
         .map_response(|response: String| format!("map_response({})", response))
         .service(svc);
-    let response = sb.oneshot("Vanilla request".to_owned()).await.unwrap();
-    assert_that!(response).is_equal_to("map_response(my_service(Vanilla request))".to_owned());
+    let response = sb.oneshot("Vanilla request").await.unwrap();
+    assert_that!(response.as_ref()).is_equal_to("map_response(my_service(Vanilla request))");
+}
+
+#[tokio::test]
+async fn middleware_with_different_return_type() {
+
+    //a service consumes a request and returns a response
+    let svc = tower::service_fn(my_service);
+    let sb = ServiceBuilder::new().service(svc);
+    let response = sb.oneshot(42).await.unwrap();
+    assert_that!(response.as_ref()).is_equal_to("my_service(42)");
 }
