0c240ee114f65d0672ff843569d45dcc58d1b660
Robert <robert.nolte@andrena.de>
2023-11-23 11:06:24 +0100

    Reorder tests

--- tests/exploratory_tower_middleware.rs
+++ tests/exploratory_tower_middleware.rs
@@ -25,18 +25,18 @@ async fn middleware_with_one_service_and_one_layer() {
 }
 
 #[tokio::test]
+async fn middleware_with_different_return_type() {
+    //a service consumes a request and returns a response
+    let sb = ServiceBuilder::new().service_fn(my_service);
+    let response = sb.oneshot(42).await.unwrap();
+    assert_that!(response.as_ref()).is_equal_to("my_service(42)");
+}
+
+#[tokio::test]
 async fn middleware_with_one_service_and_mapresponse() {
     let sb = ServiceBuilder::new()
         .map_response(|response: String| format!("map_response({})", response))
         .service_fn(my_service);
     let response = sb.oneshot("Vanilla request").await.unwrap();
     assert_that!(response.as_ref()).is_equal_to("map_response(my_service(Vanilla request))");
-}
-
-#[tokio::test]
-async fn middleware_with_different_return_type() {
-    //a service consumes a request and returns a response
-    let sb = ServiceBuilder::new().service_fn(my_service);
-    let response = sb.oneshot(42).await.unwrap();
-    assert_that!(response.as_ref()).is_equal_to("my_service(42)");
 }
